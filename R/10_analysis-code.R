2438b130f2ccf63bb4e70e09925d227ea12b22a0
Adrienne Keller <kellerab@indiana.edu>
2020-10-26 14:53:06 -0700

    revising analysis code

--- R/10_analysis-code.R
+++ R/10_analysis-code.R
@@ -29,21 +29,13 @@ hist(plot.df$rootNPercent, main = '', xlab = "% Root N", cex.lab = 1.75)
 abline(v = mean(plot.df$rootNPercent, na.rm = T), col = "red", lwd = 2)
 dev.off()
 
+# note: soil N mineral horizon is heavily right-skewed; log-transform
+
 ### bivariate relationships between N pools - cross-site
 # first check that sample sizes will be > five for each site
 sample_size_foliar<-aggregate(foliarNPercent~siteID,length,data=plot.df)
 sample_size_litter<-aggregate(litterNPercent~siteID,length,data=plot.df) # KONZ only 4
-sample_size_soil<-aggregate(soilNPercent_MHoriz~siteID,length,data=plot.df)
-# site HEAL has sample size of 1 for soil N, gets removed from analysis
+sample_size_soil<-aggregate(soilNPercent_MHoriz~siteID,length,data=plot.df) 
+# HEAL has sample size of 1 for soil N, gets removed from analysis
 #  anyways because there are no data from any of the other three pools
 
-# Litter N ~ Foliar N
-plot(litterNPercent~foliarNPercent,data=plot.df)
-foliar_to_litter_lm<-lm(litterNPercent ~ foliarNPercent,data=plot.df) 
-outlierTest(foliar_to_litter_lm) #no clear outliers, so proceed
-summary(foliar_to_litter_lm)
-#R2 = 0.31, P < 0.001, slope = 0.29
-
-
-plot.df.orig <- plot.df
-plot.df <- plot.df %>% filter(siteID != "GUAN")
