f8bf0976922f9372f4e037d960b9fcde21bb6c17
Andrew Felton <felton12392@gmail.com>
2020-12-08 17:28:43 -0600

    quick fix.

--- R/prelim_analyses.R
+++ R/prelim_analyses.R
@@ -12,22 +12,22 @@
 
 #soil and MAP
 soil.to.leaf.map.lme<-lme(foliarNPercent~soilNPercent_MHoriz + MAP,data=X,
-                          random=~1|siteID,na.action=na. omit,method="REML")
+                          random=~1|siteID,na.action=na.omit,method="REML")
 
 #soil and veg
 soil.to.leaf.veg.lme<-lme(foliarNPercent~soilNPercent_MHoriz + VEG,data=X,
-                          random=~1|siteID,na.action=na. omit,method="REML")
+                          random=~1|siteID,na.action=na.omit,method="REML")
 
 #soil and texture
 soil.to.leaf.tex.lme<-lme(foliarNPercent~soilNPercent_MHoriz + TEX,data=X,
-                          random=~1|siteID,na.action=na. omit,method="REML")
+                          random=~1|siteID,na.action=na.omit,method="REML")
 
 AIC(soil.to.leaf.map.lme, soil.to.leaf.veg.lme, soil.to.leaf.tex.lme)
 
 #full model
 
 soil.to.leaf.tex.lme<-lme(foliarNPercent~soilNPercent_MHoriz + MAP + VEG + TEX,data=X,
-                          random=~1|siteID,na.action=na. omit,method="REML")
+                          random=~1|siteID,na.action=na.omit,method="REML")
 
 
 
